94cf81fc72591a542bd19f91d568695d5e18b0cb
BrunBoy <23rreddy@athenian.org>
2020-09-16 02:56:25 +0000

    done?

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -2,13 +2,27 @@ import java.util.Scanner;
 
 public class MyMain {
     
-    public static int binaryToDecimal(String binary) { 
-        return -1;
-        // REPLACE WITH YOUR CODE HERE
+    public static int binaryToDecimal(String binary) {
+        int krumsicle=Integer.parseInt(binary);
+        String bucket=binary;
+        int meerkat=0;
+        for (int i=0; i<bucket.length(); i++){
+            if (krumsicle%10==1){
+                meerkat+=Math.pow(2,i);
+            }
+            else{
+                meerkat+=0;
+            }
+            krumsicle/=10;
+        }
+        return meerkat;
     }
     
     
     public static void main(String[] args) {
-        // YOUR CODE HERE
+        Scanner scan=new Scanner(System.in);
+        System.out.println("Gimme binary number: ");
+        String binary=scan.nextLine();
+        System.out.println("That is equivalent to: "+binaryToDecimal(binary));
     }
 }
